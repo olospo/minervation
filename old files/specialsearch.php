db0f5ccf2d4e201f91aa4f1f2cd2bb87f002a176
Tom Brooks <tom@olospo.co.uk>
2021-08-24 14:19:19 +0000

    Move old files

---
+++ old files/specialsearch.php
@@ -0,0 +1,7 @@
+<div id="sb-search" class="sb-search">
+    <form>
+        <input class="sb-search-input" placeholder="Enter your search term..." type="search" value="" name="search" id="search">
+        <input class="sb-search-submit" type="submit" value="">
+        <span class="sb-icon-search"></span>
+    </form>
+</div>
